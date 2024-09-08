7f992b2e9e16eca0f073731dc128d74933ca66b4
Nedelcu Bogdan Sebastian <nedelcu.bogdan.sebastian@gmail.com>
2024-09-08 22:31:15 +0300

    Delete obj directory

--- obj/Core/core_riscv.d
+++
@@ -1,2 +0,0 @@
-Core/core_riscv.o: \
- C:/Users/Bogdan/mrs_community_workspace3/CH32V307JSONMODBUSWEBSOCKETAJAX/Core/core_riscv.c
--- obj/base64/base64.d
+++
@@ -1,3 +0,0 @@
-base64/base64.o: ../base64/base64.c ../base64/base64.h
-
-../base64/base64.h:
